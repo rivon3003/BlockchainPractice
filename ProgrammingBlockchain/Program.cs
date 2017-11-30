5d6ad4db6e89b0cbabf49917a57396693b16635c
rivon3003 <rivon.programmer@gmail.com>
2017-11-30 08:35:13 +0700

    Bitcoin Address

---
+++ ProgrammingBlockchain/Program.cs
@@ -0,0 +1,21 @@
+﻿using ProgrammingBlockchain.Chapters;
+using System;
+
+namespace ProgrammingBlockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Select the chapter here.             
+            var chapter = new Chapter1();
+
+            //call the lesson here.             
+            chapter.Lesson4();
+
+            //this will hold the window open for you to read the output.             
+            Console.WriteLine("\n\n\nPress enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
